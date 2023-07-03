b02a6cfc5b4267a2ceec3fbbf56b1962458452a9
Ryan Fowler <ryan.fowler19@gmail.com>
2023-07-03 08:02:45 -0700

    Add Pool tests and docs

--- src/pool.rs
+++ src/pool.rs
@@ -22,8 +22,10 @@ struct State {
 }
 
 impl Pool {
-    pub async fn new(builder: ClientBuilder, max_conns: usize) -> Result<Self, Error> {
-        let opens = (0..max_conns).map(|_| builder.clone().open());
+    /// Returns a new [`Pool`] with the provided client configuration and
+    /// number of connections.
+    pub async fn new(builder: ClientBuilder, num_conns: usize) -> Result<Self, Error> {
+        let opens = (0..num_conns).map(|_| builder.clone().open());
         let clients = join_all(opens)
             .await
             .into_iter()
@@ -73,5 +75,44 @@ impl Pool {
     fn get(&self) -> &Client {
         let n = self.state.counter.fetch_add(1, Relaxed);
         &self.state.clients[n as usize % self.state.clients.len()]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test_async_macro::test_async;
+
+    #[test_async]
+    async fn test_pool() {
+        let tmp_dir = tempfile::tempdir().unwrap();
+        let builder = ClientBuilder::new().path(tmp_dir.path().join("sqlite.db"));
+        let pool = Pool::new(builder, 2)
+            .await
+            .expect("client unable to be opened");
+
+        pool.conn(|conn| {
+            conn.execute(
+                "CREATE TABLE testing (id INTEGER PRIMARY KEY, val TEXT NOT NULL)",
+                (),
+            )?;
+            conn.execute("INSERT INTO testing VALUES (1, ?)", ["value1"])
+        })
+        .await
+        .expect("writing schema and seed data");
+
+        let fs = (0..10).map(|_| {
+            pool.conn(|conn| {
+                let val: String =
+                    conn.query_row("SELECT val FROM testing WHERE id=?", [1], |row| row.get(0))?;
+                assert_eq!(val, "value1");
+                Ok(())
+            })
+        });
+        futures::future::join_all(fs)
+            .await
+            .into_iter()
+            .collect::<Result<(), Error>>()
+            .expect("collecting query results");
     }
 }
